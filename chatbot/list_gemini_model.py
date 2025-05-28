02f1ef0374465964135d15dad944d6ad2b5fb468
RAMLAH MUNIR <ramlahmunir786@gmail.com>
2025-05-28 23:58:49 +0500

    removed accessive details

--- chatbot/list_gemini_model.py
+++ chatbot/list_gemini_model.py
@@ -7,9 +7,3 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 genai.configure(api_key=GEMINI_API_KEY)
 
-# List all models
-models = genai.list_models()
-
-print("📋 Available Gemini Models:\n")
-for model in models:
-    print(f"- {model.name}  (Supports generate_content: )")
