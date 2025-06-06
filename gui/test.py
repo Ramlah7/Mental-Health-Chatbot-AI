1ab19229f20e989bdcd648102e4267fb6e8257d3
RAMLAH MUNIR <ramlahmunir786@gmail.com>
2025-06-06 15:26:49 +0500

    deleted unnecessary files

--- gui/test.py
+++
@@ -1,9 +0,0 @@
-from database import database_handler
-
-print("🔍 Attempting DB connection...")
-conn = database_handler.get_db_connection()
-print("✅ It worked!")
-schema=database_handler.init_schema()
-print("it too")
-
-conn.close()
